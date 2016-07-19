10ea796ce4def58db63d782db1332f27f4026b40
Jane Ossai <buff_jane@hotmail.com>
2016-07-19 15:31:03 +0100

    basic ionic app. making it into a reviw course thingy ..slowly

---
+++ www/js/services.js
@@ -0,0 +1,77 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    coursecode:'2322' ,
+    courseName: 'biology concepts',
+    author: 'Ben Sparrow',
+    comments: 'it was ok like?',
+    rating: '3',
+    time: '12:00',
+    date: '12/9/2016',
+    face:'img/ben.png'
+
+  }, {
+    id: 1,
+    author: 'Max Lynx',
+    coursecode:'2322' ,
+    courseName: 'stuffy concepts',
+    comments: 'You on your way?',
+    rating: '4',
+    time: '12:00',
+    date: '2/9/2016',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    author: 'Adam Bradleyson',
+    coursecode:'2322' ,
+    courseName: 'maths concepts',
+    comments: 'teacher was hilr ',
+    rating: '2',
+    time: '12:00',
+    date: '12/9/2016',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    author: 'Perry Governor',
+    coursecode:'2322' ,
+    courseName: 'science concepts',
+    comments: 'it was aight ?',
+    rating: '2',
+    time: '12:00',
+    date: '14/9/2016',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    author: 'Mike Harrington',
+    coursecode:'2322' ,
+    courseName: 'biology concepts',
+    comments: 'the course was ok , could have been beter doe',
+    rating: '5',
+    time: '12:00',
+    date: '12/9/2016',
+    face: 'img/mike.png'
+  }];
+
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
